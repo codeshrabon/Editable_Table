3ccb63531bc08979992dde40700f82bc4565babc
Faysal Ahmed <19101043@uap-bd.edu>
2025-07-11 21:13:36 +0600

    page is not getting the icon

---
+++ lib/edit_able_table.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+class EditableTable extends StatefulWidget{
+  const EditableTable({super.key});
+
+    // TODO: implement createState
+  @override
+  State<EditableTable> createState() => _EditableTableState ();
+
+}
+
+class _EditableTableState extends State<EditableTable> {
+  List<List<TextEditingController>> _controllers = [];
+  
+  int rowCount = 1;
+  int columnCount = 1;
+
+  @override
+  void initState(){
+    super.initState();
+    _initializeTable();
+  }
+  void _initializeTable(){
+    _controllers = List.generate(rowCount, (row) => List.generate(columnCount, (_) => TextEditingController(),));
+  }
+
+  //adding ROW
+  void _addRow(){
+    setState(() {
+      _controllers.add(List.generate(columnCount, (_)=> TextEditingController()));
+      rowCount++;
+    });
+  }
+  //aDDING cOLUMN
+  void _addColumn(){
+    setState(() {
+      for(var row in _controllers){
+        row.add(TextEditingController());
+      }
+      columnCount++;
+    });
+  }
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Table(
+          border: TableBorder.all(),
+          children: _controllers.map((row){
+            return TableRow(
+              children: row.map((controller){
+                return Padding(padding: const EdgeInsets.all(7.0),
+                  child: TextField(
+                    controller: controller,
+                    decoration: const InputDecoration(border: InputBorder.none),
+                  ),
+
+                );
+
+              }).toList(),
+            );
+          }).toList(),
+
+
+        ),
+        const SizedBox(height: 15,),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(onPressed: _addRow, child: const Text("Add Row")),
+            const SizedBox(width: 10,),
+            ElevatedButton(onPressed: _addColumn, child: const Text("Add Column")),
+          ],
+        )
+      ],
+    );
+  }
+
+
+  @override
+  void dispose(){
+    for(var row in _controllers){
+      for(var controller in row){
+        controller.dispose();
+      }
+    }
+    super.dispose();
+  }
+  
+}
+
+
+
---
+++ lib/test_material.dart
@@ -0,0 +1,43 @@
+import 'package:editable_table_app/main.dart';
+import 'package:flutter/material.dart';
+void main(){
+  runApp(
+    const MaterialApp(title: "Home page", home: TutorialHome()));
+}
+
+class TutorialHome extends StatelessWidget {
+  const TutorialHome ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    //throw UnimplementedError();
+    //Scaffold is a layer for the major Material Components
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(onPressed: (){},
+            //() => key.runtimeType,
+          icon: Icon(Icons.menu),
+          tooltip: 'Navigation menu',
+        ),
+        title: const Text('Editable Table'),
+
+        actions: [
+          IconButton(
+            onPressed:() {},
+            icon:Icon(Icons.search),
+            tooltip:'Search',),
+
+        ],
+      ),
+      body: const Padding(padding: EdgeInsets.all(16.0),
+      child: EditableTable(),
+      ),
+
+
+      floatingActionButton: FloatingActionButton(tooltip: 'Add',
+      onPressed: () {} ,
+      child: Icon(Icons.add),)
+    );
+  }
+}
